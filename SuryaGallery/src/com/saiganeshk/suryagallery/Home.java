fa4a4dc770610ba5a9c428622ff08ecef7a8ae08
Saiganesh K <saiganeshk@pivotsys.com>
2014-06-11 17:32:47 +0530

    Removing header bar

--- SuryaGallery/src/com/saiganeshk/suryagallery/Home.java
+++ SuryaGallery/src/com/saiganeshk/suryagallery/Home.java
@@ -21,6 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -39,6 +40,7 @@ public class Home extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
